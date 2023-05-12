02f9c2d29b5043985cf852ff0bf0871238852783
Helio Morais <heliogov20@gmail.com>
2023-05-12 19:10:13 -0300

    Requisitos 1 e 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,79 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { login } from '../redux/actions';
 
-class Login extends React.Component {
+class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleClick = () => {
+    const { dispatch, history } = this.props;
+    const { email } = this.state;
+    dispatch(login(email));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Logiiiin</div>;
+    const { email, password } = this.state;
+
+    const validateEmail = (userEmail) => {
+      const regexEmail = /\S+@\S+\.\S+/;
+      return regexEmail.test(userEmail);
+    };
+
+    const MIN_PASSWORD_LENGTH = 6;
+    const validatePassword = (userPassword) => userPassword.length >= MIN_PASSWORD_LENGTH;
+
+    return (
+      <form>
+        <label htmlFor="email-input">
+          Email:
+          <input
+            type="email"
+            id="email-input"
+            data-testid="email-input"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="password-input">
+          Senha:
+          <input
+            type="password"
+            id="password-input"
+            name="password"
+            value={ password }
+            data-testid="password-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button
+          type="button"
+          disabled={ !(validateEmail(email) && validatePassword(password)) }
+          onClick={ this.handleClick }
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
+
+export default connect()(Login);
